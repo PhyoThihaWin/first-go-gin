37b2ae07fe0b67b5ac914476d2a9f76aaf7afb6f
PhyoThihaWin <phyothiha2580@gmailc.om>
2024-02-26 13:53:07 +0630

    done: crud with go-gorm-mysql

--- crud_with_go/main.go
+++ crud_with_go/main.go
@@ -1,30 +1,81 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 
-	"example.com/m/crud_with_go/config"
 	"example.com/m/crud_with_go/models"
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
 )
 
-var db *gorm.DB
-
 func main() {
-	config.Connect()
-	db = config.GetDB()
-	db.AutoMigrate(&models.User{})
-
 	router := gin.Default()
-	router.GET("/user/list", GetUsers)
+	router.GET("/users", GetUsers)
+	router.GET("/user/:id", GetUserById)
+	router.POST("/user", CreateUser)
+	router.DELETE("/user/:id", DeleteUser)
 	router.Run("localhost:9090")
 }
 
 func GetUsers(context *gin.Context) {
-	var Users []models.User
-	db.Find(&Users)
-	// return Users
-
-	context.IndentedJSON(http.StatusOK, models.DataResponse[[]models.User]{Data: Users})
+	users := models.GetAllUsers()
+	context.IndentedJSON(http.StatusOK, models.DataResponse[[]models.User]{Data: users})
 }
+
+func GetUserById(context *gin.Context) {
+	userId := context.Param("id")
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	userDetail, _ := models.GetUserById(ID)
+	context.IndentedJSON(http.StatusOK, models.DataResponse[models.User]{Data: *userDetail})
+}
+
+func CreateUser(context *gin.Context) {
+	CreateUser := &models.User{}
+	if err := context.BindJSON(&CreateUser); err != nil {
+		context.IndentedJSON(http.StatusBadRequest, models.DataResponse[string]{Data: "Bad Error"})
+	} else {
+		data := CreateUser.CreateBook()
+		context.IndentedJSON(http.StatusOK, models.DataResponse[models.User]{Data: *data})
+	}
+}
+
+func DeleteUser(context *gin.Context) {
+	userId := context.Param("id")
+	fmt.Print("userId", userId)
+	ID, err := strconv.ParseInt(userId, 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing")
+	}
+	userDetail := models.DeleteUser(ID)
+	context.IndentedJSON(http.StatusOK, models.DataResponse[models.User]{Data: *userDetail})
+}
+
+// func UpdateBook(w http.ResponseWriter, r *http.Request) {
+// 	var updateBook = &models.Book{}
+// 	utils.ParseBody(r, updateBook)
+// 	vars := mux.Vars(r)
+// 	bookId := vars["bookId"]
+// 	ID, err := strconv.ParseInt(bookId, 0, 0)
+// 	if err != nil {
+// 		fmt.Println("error while parsing")
+// 	}
+// 	bookDetails, db := models.GetBookById(ID)
+// 	if updateBook.Name != "" {
+// 		bookDetails.Name = updateBook.Name
+// 	}
+// 	if updateBook.Author != "" {
+// 		bookDetails.Author = updateBook.Author
+// 	}
+// 	if updateBook.Publication != "" {
+// 		bookDetails.Publication = updateBook.Publication
+// 	}
+// 	db.Save(&bookDetails)
+// 	res, _ := json.Marshal(bookDetails)
+// 	w.Header().Set("Content-Type", "pkglication/json")
+// 	w.WriteHeader(http.StatusOK)
+// 	w.Write(res)
+// }
--- crud_with_go/models/user.go
+++ crud_with_go/models/user.go
@@ -1,7 +1,43 @@
 package models
 
+import (
+	"example.com/m/crud_with_go/config"
+	"gorm.io/gorm"
+)
+
 type User struct {
 	CustomModel
 	USERNAME  string `json:"username"`
 	DEVICE_ID string `json:"device_id"`
+}
+
+var db *gorm.DB
+
+func init() {
+	config.Connect()
+	db = config.GetDB()
+	db.AutoMigrate(&User{})
+}
+
+func (b *User) CreateBook() *User {
+	db.Create(&b)
+	return b
+}
+
+func GetAllUsers() []User {
+	var Users []User
+	db.Find(&Users)
+	return Users
+}
+
+func GetUserById(Id int64) (*User, *gorm.DB) {
+	var getUser User
+	db := db.Where("id=?", Id).Find(&getUser)
+	return &getUser, db
+}
+
+func DeleteUser(ID int64) *User {
+	var user User
+	db.Where("id=?", ID).Delete(&user)
+	return &user
 }
